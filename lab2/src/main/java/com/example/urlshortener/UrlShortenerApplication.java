fda4c8b270213399f79de9c5c1a54a6fbdfcb7dc
student332 <smart.saschka@yandex.ru>
2020-12-16 17:36:47 +0300

    Create UrlShortenerApplication.java

---
+++ lab2/src/main/java/com/example/urlshortener/UrlShortenerApplication.java
@@ -0,0 +1,35 @@
+package com.example.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.nio.charset.StandardCharsets;
+
+@SpringBootApplication
+public class UrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerApplication.class, args);
+	}
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		return new JedisConnectionFactory();
+	}
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+		template.setConnectionFactory(jedisConnectionFactory());
+		template.setHashKeySerializer(new StringRedisSerializer(StandardCharsets.UTF_8));
+		//template.setHashValueSerializer(new StringRedisSerializer(StandardCharsets.UTF_8));
+		template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+		return template;
+	}
+
+}
